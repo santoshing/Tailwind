d81bddaef5e3df1d3ff468198114b91bbc36a78f
santoshing <protoyshing@gmail.com>
2024-01-22 20:44:22 +0600

    git

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+       content: ["./src/**/*.{html,js}"],
+       theme: {
+              extend: {},
+       },
+       plugins: [],
+}
+// tailwind.config.js
+module.exports = {
+       // Other configuration settings
+       darkMode: false,
+}
